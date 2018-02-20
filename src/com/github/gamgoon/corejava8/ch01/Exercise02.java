8790a4fffc95913495c09e81dd901cfebadd4886
gamgoon <gamgoon@gmail.com>
2018-02-20 14:32:59 +0900

    test

---
+++ src/com/github/gamgoon/corejava8/ch01/Exercise02.java
@@ -0,0 +1,15 @@
+package com.github.gamgoon.corejava8.ch01;
+
+import java.util.Scanner;
+
+/**
+ * 정수(양의 정수 또는 음의 정수)로 된 각도를 읽고 0~369도 사이의 값으로 정규화하는 프로그램을 작성하라.
+ * 먼저 % 연산자를 이용해 만들어본 다음 floorMod 메서드를 이용해 만들어본다.
+ */
+public class Exercise02 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+
+    }
+}
